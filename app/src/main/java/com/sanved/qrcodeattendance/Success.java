8c29386ffd266879e31b072ef81d00855c1d5462
Sanved <sanved77@gmail.com>
2019-04-26 19:41:17 -0400

    success screen added

---
+++ app/src/main/java/com/sanved/qrcodeattendance/Success.java
@@ -0,0 +1,14 @@
+package com.sanved.qrcodeattendance;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.success);
+    }
+}
